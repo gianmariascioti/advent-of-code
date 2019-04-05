f04a40237bed676e53b92d1807d2effcc8c6c2e8
gianmaria scioti <jimmyscioti@gmail.com>
2019-04-05 11:02:04 +0200

    clean up

--- Day-1/main.rb
+++ Day-1/main.rb
@@ -1,9 +1,12 @@
 path = File.read('input.txt').split(' ').map(&:strip)
 
-$coordinates_array = ['N', 'E', 'S', 'W']
-$facing = 'N'
-$x = 0
-$y = 0
+# SETTING INSTANCE VARIABLES
+
+@coordinates_array = ['N', 'E', 'S', 'W']
+@facing = 'N'
+@x = 0
+@y = 0
+
 # example_1 = ['R2', 'L3']
 # example_2 = ['R2', 'R2', 'R2']
 # example_3 = ['R5, 'L5', 'R5', 'R3']
@@ -11,26 +14,26 @@ $y = 0
 # CHANGING THE DIRECTION BASED ON THE STARTING DIRECTION (N)
 
 def turn_right
-  $coordinates_array.push($coordinates_array.shift)
-  $facing = $coordinates_array[0]
+  @coordinates_array.push(@coordinates_array.shift)
+  @facing = @coordinates_array[0]
 end
 
 def turn_left
-  $coordinates_array.prepend($coordinates_array.pop)
-  $facing = $coordinates_array[0]
+  @coordinates_array.prepend(@coordinates_array.pop)
+  @facing = @coordinates_array[0]
 end
 
 # UPDATING COORDINATES BASED ON THE DIFFERENT DIRECTIONS
 
 def update_coordinates(distance)
-  if $facing == 'N'
-    $y += distance
-  elsif $facing == 'E'
-    $x += distance
-  elsif $facing == 'S'
-    $y -= distance
+  if @facing == 'N'
+    @y += distance
+  elsif @facing == 'E'
+    @x += distance
+  elsif @facing == 'S'
+    @y -= distance
   else
-    $x -= distance
+    @x -= distance
   end
 end
 
@@ -47,11 +50,11 @@ def movements(direction)
     update_coordinates(distance)
   end
 
-  easter_bunny_hq = $x.abs + $y.abs
+  easter_bunny_hq = @x.abs + @y.abs
 
   puts "Easter Bunny HQ is #{easter_bunny_hq} blocks away"
 
-  return easter_bunny_hq
+  easter_bunny_hq
 end
 
 movements(path)
